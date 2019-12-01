312ca7e1268b1ab98277f29f838b9be44617ead0
Pedro <pedro.cremasco@digiage.com.br>
2019-12-01 20:51:43 -0300

    Starting project

---
+++ src/routes/user-route.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const express = require ('express');
+const router = express.Router();
+const controller = require('../controllers/user-controller');
+
+router.post('/',controller.post);
+router.get('/',controller.get);
+router.get('/login',controller.doLogin);
+router.get('/cpf/:cpf',controller.getByCpf);
+
+module.exports = router;
